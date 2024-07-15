38c0410351b25a69a2bddea25d6e4a621e11d713
Teemu Latonen <teemu.p.latonen@gmail.com>
2024-07-15 12:37:32 +0300

    Update PKGBV2.ino
    Ehkä tän pitäis toimia paremmin näin?

--- FW/PKGBV2/PKGBV2.ino
+++ FW/PKGBV2/PKGBV2.ino
@@ -21,7 +21,8 @@ void setup() {
   button2.attachLongPressStop(Button_M2);
 
   // open the serial port:
-  Serial.begin(9600);
+  //Serial.begin(9600);
+  
   // initialize control over the keyboard:
   Keyboard.begin();
 }
@@ -31,34 +32,22 @@ void loop() {
   // keep watching the push buttons
   button1.tick();
   button2.tick();
-    // send data only when you receive data:
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    incomingString = Serial.read();
-
-    // say what you got:
-    Serial.print("I received: ");
-    Serial.println(incomingString);
-  }
 }
 
 void Button_M1()
 {
   //Keyboard.press(KEY_LEFT_GUI);
   //Keyboard.press(KEY_LEFT_SHIFT);
-  //Keyboard.write('h');
-  Keyboard.print(incomingString);
-
+  Keyboard.write('h');
   delay(10);
   Keyboard.releaseAll();
-  Serial.println("pressed home");
 }
 
 // fit to view by pressing the middle mouse button twice
 void Button_M2()
 {
   Keyboard.write('S');
-  Serial.println("pressed fit");
+  //Serial.println("pressed fit");
 }
 
 
