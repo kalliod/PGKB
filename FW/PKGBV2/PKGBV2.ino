12e9c435cd4b03b4fdb674d18fe3d81503c20949
NHamalainen <niko.hamalainen85@gmail.com>
2024-10-22 15:40:56 +0300

    updates

--- FW/PKGBV2/PKGBV2.ino
+++ FW/PKGBV2/PKGBV2.ino
@@ -1,10 +1,16 @@
 #include <Adafruit_NeoPixel.h>
 #include "Keyboard.h"
 #include <OneButton.h>
+#include <RotaryEncoder.h>
+
+
 
 // Neopixel
 #define PIN 26
 #define NUMPIXELS 11
+#define PIN_IN1 17
+#define PIN_IN2 18
+
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -23,8 +29,46 @@ OneButton button11(28, true);
 
 OneButton encoderButton(29, true);
 
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
+
 String incomingString = "S";
 String functionM1 = "";
+
+const long interval = 10; 
+unsigned long previousMillis = 0;
+
+int M1[] = {0, 20, 255};
+int M1_tmp[] = {0, 0, 0};
+
+int M2[] = {30, 128, 200};
+int M2_tmp[] = {0, 0, 0};
+
+int M3[] = {128, 128, 200};
+int M3_tmp[] = {0, 0, 0};
+
+int M4[] = {30, 50, 255};
+int M4_tmp[] = {0, 0, 0};
+
+int M5[] = {255, 128, 200};
+int M5_tmp[] = {0, 0, 0};
+
+int M6[] = {70, 70, 200};
+int M6_tmp[] = {0, 0, 0};
+
+int M7[] = {30, 128, 200};
+int M7_tmp[] = {0, 0, 0};
+
+int M8[] = {30, 255, 200};
+int M8_tmp[] = {0, 0, 0};
+
+int M9[] = {128, 255, 40};
+int M9_tmp[] = {0, 0, 0};
+
+int M10[] = {100, 128, 200};
+int M10_tmp[] = {0, 0, 0};
+
+int M11[] = {0, 255, 200};
+int M11_tmp[] = {0, 0, 0};
 
 void setup() {
 
@@ -68,6 +112,22 @@ void setup() {
 
   encoderButton.attachClick(Button_Encoder);
   encoderButton.attachLongPressStop(Button_Encoder);
+  
+ 
+
+  // Set defaul NeoPixel colors
+  pixels.setPixelColor(0, pixels.Color(M1[0], M1[1], M1[2]));
+  pixels.setPixelColor(1, pixels.Color(M2[0], M2[1], M2[2]));
+  pixels.setPixelColor(2, pixels.Color(M3[0], M3[1], M3[2]));
+  pixels.setPixelColor(3, pixels.Color(M4[0], M4[1], M4[2]));
+  pixels.setPixelColor(4, pixels.Color(M5[0], M5[1], M5[2]));
+  pixels.setPixelColor(5, pixels.Color(M6[0], M6[1], M6[2]));
+  pixels.setPixelColor(6, pixels.Color(M7[0], M7[1], M7[2]));
+  pixels.setPixelColor(7, pixels.Color(M8[0], M8[1], M8[2]));
+  pixels.setPixelColor(8, pixels.Color(M9[0], M9[1], M9[2]));
+  pixels.setPixelColor(9, pixels.Color(M10[0], M10[1], M10[2]));
+  pixels.setPixelColor(10, pixels.Color(M11[0], M11[1], M11[2]));
+  pixels.show();
 
   // open the serial port:
   //Serial.begin(9600);
@@ -77,6 +137,9 @@ void setup() {
 }
 
 void loop() {
+
+  unsigned long currentMillis = millis();
+
 
   // keep watching the push buttons
   button1.tick();
@@ -93,89 +156,360 @@ void loop() {
 
   encoderButton.tick();
 
+    if (currentMillis - previousMillis >= interval) {
+      // save the last time
+      previousMillis = currentMillis;
+      checkFade();     
+
+  }
+
+
+  static int pos = 0;
+  encoder.tick();
+
+  int newPos = encoder.getPosition();
+  if (pos < newPos) {
+    Keyboard.press(KEY_LEFT_ALT);
+    Keyboard.press(KEY_RIGHT_SHIFT);
+    Keyboard.write('d');
+
+    Keyboard.releaseAll();
+    pos = newPos;
+  }
+  if (pos > newPos) {
+    Keyboard.press(KEY_LEFT_ALT);
+    Keyboard.press(KEY_RIGHT_SHIFT);
+    Keyboard.write('i');
+
+    Keyboard.releaseAll();
+    pos = newPos;
+  }
+
 }
 
 void Button_M1()
 {
-  //Keyboard.press(KEY_LEFT_GUI);
-  //Keyboard.press(KEY_LEFT_SHIFT);
-  Keyboard.write('1');
-  //delay(10);
-  //Keyboard.releaseAll();
-  pixels.setPixelColor(0, pixels.Color(0, 150, 0));
+  M1_tmp[0] = 0;
+  M1_tmp[1] = 0;
+  M1_tmp[2] = 0;
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.write('l');
+  Keyboard.releaseAll();
+  pixels.setPixelColor(0, pixels.Color(M1_tmp[0], M1_tmp[1], M1_tmp[2]));
   pixels.show();
 }
 
-// fit to view by pressing the middle mouse button twice
 void Button_M2()
 {
+  M2_tmp[0] = 0;
+  M2_tmp[1] = 0;
+  M2_tmp[2] = 0;
   Keyboard.write('2');
-  pixels.setPixelColor(1, pixels.Color(0, 150, 0));
+  pixels.setPixelColor(1, pixels.Color(M2_tmp[0], M2_tmp[1], M2_tmp[2]));
   pixels.show();
-  //Serial.println("pressed fit");
 }
 
 void Button_M3()
 {
+  M3_tmp[0] = 0;
+  M3_tmp[1] = 0;
+  M3_tmp[2] = 0;
   Keyboard.write('3');
-  pixels.setPixelColor(2, pixels.Color(150, 0, 0));
+  pixels.setPixelColor(2, pixels.Color(M3_tmp[0], M3_tmp[1], M3_tmp[2]));
   pixels.show();
 }
 
 void Button_M4()
 {
+  M4_tmp[0] = 0;
+  M4_tmp[1] = 0;
+  M4_tmp[2] = 0;
   Keyboard.write('4');
-  pixels.setPixelColor(3, pixels.Color(0, 0, 150));
+  pixels.setPixelColor(3, pixels.Color(M4_tmp[0], M4_tmp[1], M4_tmp[2]));
   pixels.show();
 }
 
 void Button_M5()
 {
+  M5_tmp[0] = 0;
+  M5_tmp[1] = 0;
+  M5_tmp[2] = 0;
   Keyboard.write('5');
-  pixels.setPixelColor(4, pixels.Color(150, 150, 150));
+  pixels.setPixelColor(4, pixels.Color(M5_tmp[0], M5_tmp[1], M5_tmp[2]));
   pixels.show();
 }
 void Button_M6()
 {
+  M6_tmp[0] = 0;
+  M6_tmp[1] = 0;
+  M6_tmp[2] = 0;
   Keyboard.write('6');
-  pixels.setPixelColor(5, pixels.Color(50, 50, 50));
+  pixels.setPixelColor(5, pixels.Color(M6_tmp[0], M6_tmp[1], M6_tmp[2]));
   pixels.show();
 }
 void Button_M7()
 {
+  M7_tmp[0] = 0;
+  M7_tmp[1] = 0;
+  M7_tmp[2] = 0;
   Keyboard.write('7');
-  pixels.setPixelColor(6, pixels.Color(30, 30, 30));
+  pixels.setPixelColor(6, pixels.Color(M7_tmp[0], M7_tmp[1], M7_tmp[2]));
   pixels.show();
 }
 void Button_M8()
 {
+  M8_tmp[0] = 0;
+  M8_tmp[1] = 0;
+  M8_tmp[2] = 0;
   Keyboard.write('8');
-  pixels.setPixelColor(7, pixels.Color(0, 0, 255));
+  pixels.setPixelColor(7, pixels.Color(M8_tmp[0], M8_tmp[1], M8_tmp[2]));
   pixels.show();
 }
 void Button_M9()
 {
+  M9_tmp[0] = 0;
+  M9_tmp[1] = 0;
+  M9_tmp[2] = 0;
   Keyboard.write('9');
-  pixels.setPixelColor(8, pixels.Color(0, 255, 0));
+  pixels.setPixelColor(8, pixels.Color(M9_tmp[0], M9_tmp[1], M9_tmp[2]));
   pixels.show();
 }
 void Button_M10()
 {
+  M10_tmp[0] = 0;
+  M10_tmp[1] = 0;
+  M10_tmp[2] = 0;
   Keyboard.write('A');
-  pixels.setPixelColor(9, pixels.Color(255, 0, 0));
+  pixels.setPixelColor(9, pixels.Color(M10_tmp[0], M10_tmp[1], M10_tmp[2]));
   pixels.show();
 }
 void Button_M11()
 {
+  M11_tmp[0] = 0;
+  M11_tmp[1] = 0;
+  M11_tmp[2] = 0;
   Keyboard.write('B');
-  pixels.setPixelColor(10, pixels.Color(50, 150, 50));
+  pixels.setPixelColor(10, pixels.Color(M11_tmp[0], M11_tmp[1], M11_tmp[2]));
   pixels.show();
 }
 void Button_Encoder()
 {
   Keyboard.write('E');
-  pixels.clear();
-  pixels.show();
+  //pixels.clear();
+  //pixels.show();
+}
+
+void checkFade() {
+ 
+// M1 Button
+  if (M1_tmp[0] < M1[0]) {
+        M1_tmp[0]++;
+        pixels.setPixelColor(0, pixels.Color(M1_tmp[0], M1_tmp[1], M1_tmp[2]));
+        pixels.show();
+      }
+
+  if (M1_tmp[1] < M1[1]) {
+        M1_tmp[1]++;
+        pixels.setPixelColor(0, pixels.Color(M1_tmp[0], M1_tmp[1], M1_tmp[2]));
+        pixels.show();
+      }
+
+  if (M1_tmp[2] < M1[2]) {
+        M1_tmp[2]++;
+        pixels.setPixelColor(0, pixels.Color(M1_tmp[0], M1_tmp[1], M1_tmp[2]));
+        pixels.show();
+      }
+  
+  // M2 Button
+  if (M2_tmp[0] < M2[0]) {
+        M2_tmp[0]++;
+        pixels.setPixelColor(1, pixels.Color(M2_tmp[0], M2_tmp[1], M2_tmp[2]));
+        pixels.show();
+      }
+
+  if (M2_tmp[1] < M2[1]) {
+        M2_tmp[1]++;
+        pixels.setPixelColor(1, pixels.Color(M2_tmp[0], M2_tmp[1], M2_tmp[2]));
+        pixels.show();
+      }
+
+  if (M2_tmp[2] < M2[2]) {
+        M2_tmp[2]++;
+        pixels.setPixelColor(1, pixels.Color(M2_tmp[0], M2_tmp[1], M2_tmp[2]));
+        pixels.show();
+      }
+
+  // M3 Button
+  if (M3_tmp[0] < M3[0]) {
+        M3_tmp[0]++;
+        pixels.setPixelColor(2, pixels.Color(M3_tmp[0], M3_tmp[1], M3_tmp[2]));
+        pixels.show();
+      }
+
+  if (M3_tmp[1] < M3[1]) {
+        M3_tmp[1]++;
+        pixels.setPixelColor(2, pixels.Color(M3_tmp[0], M3_tmp[1], M3_tmp[2]));
+        pixels.show();
+      }
+
+  if (M3_tmp[2] < M3[2]) {
+        M3_tmp[2]++;
+        pixels.setPixelColor(2, pixels.Color(M3_tmp[0], M3_tmp[1], M3_tmp[2]));
+        pixels.show();
+      }
+
+  // M4 Button
+  if (M4_tmp[0] < M4[0]) {
+        M4_tmp[0]++;
+        pixels.setPixelColor(3, pixels.Color(M4_tmp[0], M4_tmp[1], M4_tmp[2]));
+        pixels.show();
+      }
+
+  if (M4_tmp[1] < M4[1]) {
+        M4_tmp[1]++;
+        pixels.setPixelColor(3, pixels.Color(M4_tmp[0], M4_tmp[1], M4_tmp[2]));
+        pixels.show();
+      }
+
+  if (M4_tmp[2] < M4[2]) {
+        M4_tmp[2]++;
+        pixels.setPixelColor(3, pixels.Color(M4_tmp[0], M4_tmp[1], M4_tmp[2]));
+        pixels.show();
+      }
+
+  // M5 Button
+  if (M5_tmp[0] < M5[0]) {
+        M5_tmp[0]++;
+        pixels.setPixelColor(4, pixels.Color(M5_tmp[0], M5_tmp[1], M5_tmp[2]));
+        pixels.show();
+      }
+
+  if (M5_tmp[1] < M5[1]) {
+        M5_tmp[1]++;
+        pixels.setPixelColor(4, pixels.Color(M5_tmp[0], M5_tmp[1], M5_tmp[2]));
+        pixels.show();
+      }
+
+  if (M5_tmp[2] < M5[2]) {
+        M5_tmp[2]++;
+        pixels.setPixelColor(4, pixels.Color(M5_tmp[0], M5_tmp[1], M5_tmp[2]));
+        pixels.show();
+      }
+
+  // M6 Button
+  if (M6_tmp[0] < M6[0]) {
+        M6_tmp[0]++;
+        pixels.setPixelColor(5, pixels.Color(M6_tmp[0], M6_tmp[1], M6_tmp[2]));
+        pixels.show();
+      }
+
+  if (M6_tmp[1] < M6[1]) {
+        M6_tmp[1]++;
+        pixels.setPixelColor(5, pixels.Color(M6_tmp[0], M6_tmp[1], M6_tmp[2]));
+        pixels.show();
+      }
+
+  if (M6_tmp[2] < M6[2]) {
+        M6_tmp[2]++;
+        pixels.setPixelColor(5, pixels.Color(M6_tmp[0], M6_tmp[1], M6_tmp[2]));
+        pixels.show();
+      }
+
+  // M7 Button
+  if (M7_tmp[0] < M7[0]) {
+        M7_tmp[0]++;
+        pixels.setPixelColor(6, pixels.Color(M7_tmp[0], M7_tmp[1], M7_tmp[2]));
+        pixels.show();
+      }
+
+  if (M7_tmp[1] < M7[1]) {
+        M7_tmp[1]++;
+        pixels.setPixelColor(6, pixels.Color(M7_tmp[0], M7_tmp[1], M7_tmp[2]));
+        pixels.show();
+      }
+
+  if (M7_tmp[2] < M7[2]) {
+        M7_tmp[2]++;
+        pixels.setPixelColor(6, pixels.Color(M7_tmp[0], M7_tmp[1], M7_tmp[2]));
+        pixels.show();
+      }
+  
+  // M8 Button
+  if (M8_tmp[0] < M8[0]) {
+        M8_tmp[0]++;
+        pixels.setPixelColor(7, pixels.Color(M8_tmp[0], M8_tmp[1], M8_tmp[2]));
+        pixels.show();
+      }
+
+  if (M8_tmp[1] < M8[1]) {
+        M8_tmp[1]++;
+        pixels.setPixelColor(7, pixels.Color(M8_tmp[0], M8_tmp[1], M8_tmp[2]));
+        pixels.show();
+      }
+
+  if (M8_tmp[2] < M8[2]) {
+        M8_tmp[2]++;
+        pixels.setPixelColor(7, pixels.Color(M8_tmp[0], M8_tmp[1], M8_tmp[2]));
+        pixels.show();
+      }
+
+  // M9 Button
+  if (M9_tmp[0] < M9[0]) {
+        M9_tmp[0]++;
+        pixels.setPixelColor(8, pixels.Color(M9_tmp[0], M9_tmp[1], M9_tmp[2]));
+        pixels.show();
+      }
+
+  if (M9_tmp[1] < M9[1]) {
+        M9_tmp[1]++;
+        pixels.setPixelColor(8, pixels.Color(M9_tmp[0], M9_tmp[1], M9_tmp[2]));
+        pixels.show();
+      }
+
+  if (M9_tmp[2] < M9[2]) {
+        M9_tmp[2]++;
+        pixels.setPixelColor(8, pixels.Color(M9_tmp[0], M9_tmp[1], M9_tmp[2]));
+        pixels.show();
+      }
+
+  // M10 Button
+  if (M10_tmp[0] < M10[0]) {
+        M10_tmp[0]++;
+        pixels.setPixelColor(9, pixels.Color(M10_tmp[0], M10_tmp[1], M10_tmp[2]));
+        pixels.show();
+      }
+
+  if (M10_tmp[1] < M10[1]) {
+        M10_tmp[1]++;
+        pixels.setPixelColor(9, pixels.Color(M10_tmp[0], M10_tmp[1], M10_tmp[2]));
+        pixels.show();
+      }
+
+  if (M10_tmp[2] < M10[2]) {
+        M10_tmp[2]++;
+        pixels.setPixelColor(9, pixels.Color(M10_tmp[0], M10_tmp[1], M10_tmp[2]));
+        pixels.show();
+      }
+
+  // M11 Button
+  if (M11_tmp[0] < M11[0]) {
+        M11_tmp[0]++;
+        pixels.setPixelColor(10, pixels.Color(M11_tmp[0], M11_tmp[1], M11_tmp[2]));
+        pixels.show();
+      }
+
+  if (M11_tmp[1] < M11[1]) {
+        M11_tmp[1]++;
+        pixels.setPixelColor(10, pixels.Color(M11_tmp[0], M11_tmp[1], M11_tmp[2]));
+        pixels.show();
+      }
+
+  if (M11_tmp[2] < M11[2]) {
+        M11_tmp[2]++;
+        pixels.setPixelColor(10, pixels.Color(M11_tmp[0], M11_tmp[1], M11_tmp[2]));
+        pixels.show();
+      }
+
 }
 
 
